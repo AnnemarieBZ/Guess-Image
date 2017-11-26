57155ff89dfa4178b00f57d378bf26a5b15f2339
Annemarie Braun <zababraun@hotmail.com>
2017-11-25 18:06:43 -0600

    Add files via upload

---
+++ Guess Image/guessimage.js
@@ -0,0 +1,25 @@
+window.onload = init;
+
+        function init() {
+            var images = document.getElementsByTagName("img");
+            for (var i = 0; i < images.length; i++) {
+                images[i].onmouseover = showAnswer;
+                images[i].onmouseout = reblur;
+
+            }
+        }
+
+        function showAnswer(e) {
+            var image = e.target;
+            var name = image.id;
+            name = "imgs/" + name + ".jpg";
+            image.src = name;
+
+        }
+
+        function reblur(e) {
+            var image = e.target;
+            var name = image.id;
+            name = "imgs/" + name + "blur.jpg";
+            image.src = name;
+        }
